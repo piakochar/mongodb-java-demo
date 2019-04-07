244536aa424e5611c72dc97c1c25fbd9f72c71f0
piakochar <piakochar11@gmail.com>
2019-04-07 16:51:11 -0400

    generalize connection string

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static String MONGODB_CONNECTION_STRING = "mongodb+srv://user:pass@bechdelcluster-1y9u3.mongodb-dev.net/test";
+    public static String MONGODB_CONNECTION_STRING = "<YOUR-CONNECTION-STRING>";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
